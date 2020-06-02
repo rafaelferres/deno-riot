c49fcdd87ed2392afcc8034656da3e4b533635ae
rafaelferres <raferres@outlook.com.br>
2020-06-02 00:11:19 -0300

    test.ts

--- test.ts
+++ test.ts
@@ -8,11 +8,11 @@ riot.summoner.getSummonerByName("Plankson", Region.BR1).then((data) => {
 
 riot.summoner.getSummonerByAccountId("qaUljVmzA3WVJn8k6uztwFGyDwBWDmH5J2HA3wK2Xxw336A", Region.BR1).then((data) =>{
     console.log(data);
-})
+});
 
 riot.summoner.getSummonerByPuuid("y-lJrZX8zV_-0guWmG1kCNd3b1doBu3avSC5s5c0xva5k8x8v5328qXoqCuJwb6ICgqhA__7d7od1A", Region.BR1).then((data) =>{
     console.log(data);
-})
+});
 
 riot.summoner.getSummonerByEncryptedSummonerId("mMJZj2mbolYcL0NX1JJIM78V7irhTa92L77E1Vf2RHx3hA", Region.BR1).then((data) => {
     console.log(data);
@@ -24,15 +24,15 @@ riot.spectator.getCurrentGameByEncryptedSummonerId("mMJZj2mbolYcL0NX1JJIM78V7irh
 
 riot.spectator.getFeaturedGames(Region.BR1).then((data) => {
     console.log(data);
-})
+});
 
 riot.ranked.getLeaderboards(LeadboardRegion.Americas).then((data) => {
     console.log(data);
-})
+});
 
 riot.status.getShardData(Region.BR1).then((data) => {
     console.log(data);
-})
+});
 
 riot.champion.getChampionRotations(Region.BR1).then((data) => {
     console.log(data);
@@ -56,7 +56,7 @@ riot.championMastery.getScoreByEncryptedSummonerId("mMJZj2mbolYcL0NX1JJIM78V7irh
 
 riot.exp.getAllLeague(Queue.RankedSolo5x5, Tier.Gold, Division.III, Region.BR1).then((data) => {
     console.log(data);
-})
+});
 
 riot.clash.getPlayer("mMJZj2mbolYcL0NX1JJIM78V7irhTa92L77E1Vf2RHx3hA", Region.BR1).then((data) => {
     console.log(data);
@@ -68,7 +68,7 @@ riot.clash.getAllTournaments(Region.BR1).then((data) => {
 
 riot.clash.getTeamById("DqndknkqwNJKDX", Region.BR1).then((data) => {
     console.log(data);
-})
+});
 
 riot.clash.getTournamentsByTeamId("oidqwd4q4dqw484", Region.BR1).then((data) => {
     console.log(data);
@@ -80,7 +80,7 @@ riot.league.getChallangerLeagueByQueue(Queue.RankedSolo5x5, Region.BR1).then((da
 
 riot.league.getLeagueBySummonerId("mMJZj2mbolYcL0NX1JJIM78V7irhTa92L77E1Vf2RHx3hA", Region.BR1).then((data) => {
     console.log(data);
-});;
+});
 
 riot.league.getAllLeagueEntries(Queue.RankedSolo5x5, Tier.Diamond, Division.III, Region.BR1).then((data) => {
     console.log(data);
@@ -88,7 +88,7 @@ riot.league.getAllLeagueEntries(Queue.RankedSolo5x5, Tier.Diamond, Division.III,
 
 riot.league.getGrandmasterLeagueByQueue(Queue.RankedSolo5x5, Region.BR1).then((data) => {
     console.log(data);
-})
+});
 
 riot.league.getLeagueById("qwdmDKWkdqkwjndkjqnk", Region.BR1).then((data) => {
     console.log(data);
@@ -96,4 +96,4 @@ riot.league.getLeagueById("qwdmDKWkdqkwjndkjqnk", Region.BR1).then((data) => {
 
 riot.league.getMasterLeagueByQueue(Queue.RankedSolo5x5, Region.BR1).then((data) => {
     console.log(data);
-})
+});
